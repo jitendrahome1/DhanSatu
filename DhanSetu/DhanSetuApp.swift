b279394d6bd924926281d4d650457733c771b40c
agarwji <jitendra.agarwal@in.verizon.com>
2025-06-23 18:17:45 +0530

    Initial Commit

---
+++ DhanSetu/DhanSetuApp.swift
@@ -0,0 +1,17 @@
+//
+//  DhanSetuApp.swift
+//  DhanSetu
+//
+//  Created by Jitendra Agarwal on 23/06/25.
+//
+
+import SwiftUI
+
+@main
+struct DhanSetuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
